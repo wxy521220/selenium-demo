9b08e655c48a2bd74122cb9ba6b4d7781472a3ee
wang_xy <wang_xy1@murongtech.com>
2020-12-03 11:25:37 +0800

    修改为相应的yaml文件类名

--- src/test/java/com/framework01/ParamsTest.java
+++ src/test/java/com/framework01/ParamsTest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
 import java.util.stream.Stream;
@@ -20,22 +19,22 @@ public class ParamsTest {
 
     @ParameterizedTest
     @MethodSource
-    void search_04(TestCase testCase) {
+    void search_04(SearchStep searchStep) {
 
-        System.out.println(testCase);
-        testCase.run();
+        System.out.println(searchStep);
+        searchStep.run();
     }
 
 
 
-    static Stream<TestCase> search_04() throws IOException {
+    static Stream<SearchStep> search_04() throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         //TypeReference typeReference=new TypeReference<List<String>>(){};
 
-        TestCase testCase = mapper.readValue(
+        SearchStep searchStep = mapper.readValue(
                 ParamsTest.class.getResourceAsStream("/searchStep.yaml"),
-                TestCase.class);
-        return Stream.of(testCase);
+                SearchStep.class);
+        return Stream.of(searchStep);
 
     }
 
--- src/test/java/com/framework01/SearchStep.java
+++ src/test/java/com/framework01/SearchStep.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class TestCase {
+public class SearchStep {
     public List<String> data;
     public List<HashMap<String,Object>> steps;
     static WebDriver driver;
