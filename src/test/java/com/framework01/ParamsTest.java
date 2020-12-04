80709f8381bc47764deaa6c3c88032afde85b252
wang_xy <wang_xy1@murongtech.com>
2020-12-04 23:56:24 +0800

    case裂变，参数化

--- src/test/java/com/framework01/ParamsTest.java
+++ src/test/java/com/framework01/ParamsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -21,20 +22,21 @@ public class ParamsTest {
     @MethodSource
     void search_04(SearchStep searchStep) {
 
-        System.out.println(searchStep);
         searchStep.run();
+        //System.out.println(searchStep);
+
     }
 
 
 
-    static Stream<SearchStep> search_04() throws IOException {
+    static List<SearchStep> search_04() throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         //TypeReference typeReference=new TypeReference<List<String>>(){};
 
         SearchStep searchStep = mapper.readValue(
                 ParamsTest.class.getResourceAsStream("/searchStep.yaml"),
                 SearchStep.class);
-        return Stream.of(searchStep);
+        return searchStep.testcaseGenerate();
 
     }
 
