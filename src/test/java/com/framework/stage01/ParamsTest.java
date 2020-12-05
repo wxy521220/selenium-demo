6a0b1181b826c6e5eb39f16d72ddf050dd8140d8
wang_xy <wang_xy1@murongtech.com>
2020-12-05 11:05:25 +0800

    打造测试框架-分阶段提交

---
+++ src/test/java/com/framework/stage01/ParamsTest.java
@@ -0,0 +1,47 @@
+package com.framework.stage01;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+/**
+ * 测试步骤驱动
+ */
+
+
+public class ParamsTest {
+
+
+    @ParameterizedTest
+    @MethodSource
+    void search_04(SearchStep searchStep) {
+
+        System.out.println(searchStep);
+        searchStep.run();
+    }
+
+
+
+    static Stream<SearchStep> search_04() throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        //TypeReference typeReference=new TypeReference<List<String>>(){};
+
+        SearchStep searchStep = mapper.readValue(
+                ParamsTest.class.getResourceAsStream("/searchStep.yaml"),
+                SearchStep.class);
+        return Stream.of(searchStep);
+
+    }
+
+
+
+
+
+
+
+}
